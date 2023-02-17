2201536074b0a9e12ed0bd1e4bbba0827311dc3f
SBrown27 <113198958+SBrown27@users.noreply.github.com>
2023-02-17 15:11:40 -0500

    Update gradebook.cpp
    Fixed header file capitalization in the function names.

--- CPP_Files/gradebook.cpp
+++ CPP_Files/gradebook.cpp
@@ -1,4 +1,4 @@
-#include "Gradebook.h"
+#include "gradebook.h"
 #include <iostream>
 #include <string>
 #include<vector>
@@ -7,143 +7,143 @@
 
 
 //Setters
-void Gradebook::set_name(std::string student_name)
+void gradebook::set_name(std::string student_name)
 {
 	name = student_name;
 }
 
 //
-void Gradebook::set_lab_names(std::vector<std::string> names_of_labs)
+void gradebook::set_lab_names(std::vector<std::string> names_of_labs)
 {
 	lab_names = names_of_labs;
 }
 
 //
-void Gradebook::set_assignment_names(std::vector<std::string> names_of_assignments)
+void gradebook::set_assignment_names(std::vector<std::string> names_of_assignments)
 {
 	assignment_names = names_of_assignments;
 }
 
 //
-void Gradebook::set_project_names(std::vector<std::string> names_of_projects)
+void gradebook::set_project_names(std::vector<std::string> names_of_projects)
 {
 	project_names = names_of_projects;
 }
 
 //
-void Gradebook::set_exam_name(std::string name_of_exam)
+void gradebook::set_exam_name(std::string name_of_exam)
 {
 	exam_name = name_of_exam;
-	
+
 
 }
 
 
 //
-void Gradebook::set_lab_grades(std::vector<float> grades)
+void gradebook::set_lab_grades(std::vector<float> grades)
 {
 	lab_grades = grades;
 }
 
 //
-void Gradebook::set_assignment_grades(std::vector<float> grades)
+void gradebook::set_assignment_grades(std::vector<float> grades)
 {
 	assignment_grades = grades;
 }
 
 //
-void Gradebook::set_project_grades(std::vector<float> grades)
+void gradebook::set_project_grades(std::vector<float> grades)
 {
 	project_grades = grades;
 }
 
 //
-void Gradebook::set_exam_grade(float grade)
+void gradebook::set_exam_grade(float grade)
 {
 	exam_grade = grade;
 
 }
 //
-void Gradebook::set_total_grade(float grade)
+void gradebook::set_total_grade(float grade)
 {
 	total_grade = grade;
 }
 //
-void Gradebook::set_letter_grade(char grade)
+void gradebook::set_letter_grade(char grade)
 {
 	letter_grade = grade;
 }
 
 //This is all the getters
-std::string Gradebook::get_name()
+std::string gradebook::get_name()
 {
 	return name;
 
 }
 
-std::vector<std::string> Gradebook::get_lab_names()
+std::vector<std::string> gradebook::get_lab_names()
 {
 
 	return lab_names;
 }
-std::vector<std::string> Gradebook::get_assignment_names()
+std::vector<std::string> gradebook::get_assignment_names()
 {
 	return assignment_names;
 }
-std::vector<std::string> Gradebook::get_project_name()
+std::vector<std::string> gradebook::get_project_name()
 {
 
 	return project_names;
 }
-std::string Gradebook::get_exam_name()
+std::string gradebook::get_exam_name()
 {
 	return exam_name;
 }
 
-std::vector<float> Gradebook::get_lab_grades()
+std::vector<float> gradebook::get_lab_grades()
 {
 
 	return lab_grades;
 }
-std::vector<float> Gradebook::get_assignment_grades()
+std::vector<float> gradebook::get_assignment_grades()
 {
 
 	return assignment_grades;
 }
-std::vector<float> Gradebook::get_project_grades()
+std::vector<float> gradebook::get_project_grades()
 {
 	return project_grades;
 }
 
-float Gradebook::get_exam_grade()
+float gradebook::get_exam_grade()
 {
 
 	return exam_grade;
 }
-float Gradebook::get_total_grade()
+float gradebook::get_total_grade()
 {
 
 	return total_grade;
 }
-char Gradebook::get_letter_grade()
+char gradebook::get_letter_grade()
 {
 
 	return letter_grade;
 }
-void drop_lowest_labs()
+void gradebook::drop_lowest_labs()
 {
 	//This function should sort the vector of labs, then drop the lowest 2;
 }
 //The display functions
-void Gradebook::display_individual()
+void gradebook::display_individual()
 {
 	std::cout << name;
 }
-void Gradebook::display_category()
+void gradebook::display_category()
 {
-	
+
 }
-void Gradebook::display_course()
+void gradebook::display_course()
 {
 
 }
